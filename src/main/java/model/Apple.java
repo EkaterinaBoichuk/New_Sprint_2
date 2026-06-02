a1a123267d4d27a861b2cdbb0162edf62135a385
EkaterinaBoichuk <theslayercatkate@gmail.com>
2026-06-02 11:59:41 +0500

    add const

--- src/main/java/model/Apple.java
+++ src/main/java/model/Apple.java
@@ -3,6 +3,8 @@ package model;
 import model.constants.Discount;
 
 public class Apple extends Food implements Discountable {
+
+    private static final String RED = "red";
     private String colour;
     public Apple (int amount, double price, String colour) {
         super(amount, price,true);
@@ -10,11 +12,11 @@ public class Apple extends Food implements Discountable {
     }
 
     public double getDiscount() {
-        if (colour .equals("red")) {
+        if (colour.equals(RED)) {
             return Discount.FOR_RED;
         } else {
             return 0;
         }
-        }
     }
+}
 
